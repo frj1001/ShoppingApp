b5576bc727d9a2ab315a4f3d8086ad895096c85d
Farhaj Amin <farhajameen@gmail.com>
2022-09-21 19:16:10 +0500

    all

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className='header'>
+        <h1>I am the Farhaj</h1>
+      </div>
+    )
+  }
+}
